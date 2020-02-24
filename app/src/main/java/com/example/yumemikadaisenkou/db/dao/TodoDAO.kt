03fa5f178fd6b47bc22c45b03c56e22bc555d9da
門田朋己 <monndenn15@gmail.com>
2020-02-24 09:39:04 +0900

    add Query

--- app/src/main/java/com/example/yumemikadaisenkou/db/dao/TodoDAO.kt
+++ app/src/main/java/com/example/yumemikadaisenkou/db/dao/TodoDAO.kt
@@ -1,2 +1,32 @@
 package com.example.yumemikadaisenkou.db.dao
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.yumemikadaisenkou.db.entity.Todo
+
+@Dao
+interface TodoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun createTodo(todo: Todo)
+
+    @Insert
+    fun insert(todo: Todo)
+
+    @Query("SELECT * FROM todo_table")
+    fun findAll(): LiveData<List<Todo>>
+
+    @Query("SELECT * FROM todo_table WHERE completed = :type")
+    fun findSelect(type: Int): LiveData<List<Todo>>
+
+    @Query("DELETE FROM todo_table")
+    fun deleteAll()
+
+    @Update
+    fun update(todo: Todo)
+
+    @Delete
+    fun delete(todo: Todo)
+
+    @Query("DELETE FROM todo_table WHERE completed = 1")
+    fun deleteCompleted()
+}
