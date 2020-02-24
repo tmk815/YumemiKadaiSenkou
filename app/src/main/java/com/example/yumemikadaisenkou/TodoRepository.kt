e6c53d8167974f611714f8cd912a62513261e327
門田朋己 <monndenn15@gmail.com>
2020-02-24 09:48:33 +0900

    add initialize

--- app/src/main/java/com/example/yumemikadaisenkou/TodoRepository.kt
+++ app/src/main/java/com/example/yumemikadaisenkou/TodoRepository.kt
@@ -1,2 +1,19 @@
 package com.example.yumemikadaisenkou
 
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.example.yumemikadaisenkou.db.TodoDatabase
+import com.example.yumemikadaisenkou.db.dao.TodoDao
+import com.example.yumemikadaisenkou.db.entity.Todo
+
+class TodoRepository(application: Application) {
+    private val todoDao: TodoDao
+
+    private val allTodos: LiveData<List<Todo>>
+
+    init {
+        val database: TodoDatabase = TodoDatabase.getInstance(application.applicationContext)!!
+        todoDao = database.todoDao()
+        allTodos = todoDao.findAll()
+    }
+}
