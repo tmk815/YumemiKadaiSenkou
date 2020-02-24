f2b2e74b6b3649b58ad4d08d82852b849107eed2
門田朋己 <monndenn15@gmail.com>
2020-02-24 09:46:14 +0900

    add extends RoomDatabase abstract class

--- app/src/main/java/com/example/yumemikadaisenkou/db/TodoDatabase.kt
+++ app/src/main/java/com/example/yumemikadaisenkou/db/TodoDatabase.kt
@@ -1,2 +1,32 @@
 package com.example.yumemikadaisenkou.db
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.yumemikadaisenkou.db.dao.TodoDao
+import com.example.yumemikadaisenkou.db.entity.Todo
+
+@Database(entities = [Todo::class], version = 1)
+abstract class TodoDatabase : RoomDatabase() {
+    abstract fun todoDao(): TodoDao
+
+    companion object {
+        private var instance: TodoDatabase? = null
+
+        fun getInstance(context: Context): TodoDatabase? {
+            if (instance == null) {
+                synchronized(TodoDatabase::class) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        TodoDatabase::class.java, "todos_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                }
+            }
+            return instance
+        }
+    }
+
+}
