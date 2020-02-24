5cd8a959ad77916febf3c2a5d4dc9219653f14de
門田朋己 <monndenn15@gmail.com>
2020-02-24 09:35:55 +0900

    add Define database

--- app/src/main/java/com/example/yumemikadaisenkou/db/entity/Todo.kt
+++ app/src/main/java/com/example/yumemikadaisenkou/db/entity/Todo.kt
@@ -1,2 +1,13 @@
 package com.example.yumemikadaisenkou.db.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todo_table")
+data class Todo(
+    var completed: Int,
+    var todoText: String
+) {
+    @PrimaryKey(autoGenerate = true)
+    var _id: Int = 0
+}
