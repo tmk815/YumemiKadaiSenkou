52c5e06a07789b4fb624ae72b862cca3a6532dd7
門田朋己 <monndenn15@gmail.com>
2020-02-24 10:42:12 +0900

    add CURD

--- app/src/main/java/com/example/yumemikadaisenkou/TodoRepository.kt
+++ app/src/main/java/com/example/yumemikadaisenkou/TodoRepository.kt
@@ -1,6 +1,7 @@
 package com.example.yumemikadaisenkou
 
 import android.app.Application
+import android.os.AsyncTask
 import androidx.lifecycle.LiveData
 import com.example.yumemikadaisenkou.db.TodoDatabase
 import com.example.yumemikadaisenkou.db.dao.TodoDao
@@ -15,5 +16,69 @@ class TodoRepository(application: Application) {
         val database: TodoDatabase = TodoDatabase.getInstance(application.applicationContext)!!
         todoDao = database.todoDao()
         allTodos = todoDao.findAll()
+    }
+
+    fun insert(todo: Todo) {
+        val insertTodoAsyncTask = InsertTodoAsyncTask(todoDao).execute(todo)
+    }
+
+    fun deleteCompleted() {
+        val DeleteCompletedTodosAsyncTask = DeleteCompletedTodosAsyncTask(todoDao).execute()
+    }
+
+    fun deleteAll() {
+        val DeleteAllAsyncTask = DeleteAllAsyncTask(todoDao).execute()
+    }
+
+    fun findAll(): LiveData<List<Todo>> {
+        return allTodos
+    }
+
+    fun findSelect(type: Int) : LiveData<List<Todo>> {
+        return todoDao.findSelect(type)
+    }
+
+    fun update(todo: Todo) {
+        val updateTodoAsyncTask = UpdateTodoAsyncTask(todoDao).execute(todo)
+    }
+
+    fun delete(todo: Todo) {
+        val DeleteTodoAsyncTask = DeleteTodoAsyncTask(todoDao).execute(todo)
+    }
+
+    private class InsertTodoAsyncTask(val todoDao: TodoDao) : AsyncTask<Todo, Unit, Unit>() {
+
+        override fun doInBackground(vararg p0: Todo?) {
+            todoDao.insert(p0[0]!!)
+        }
+    }
+
+    private class UpdateTodoAsyncTask(val todoDao: TodoDao) : AsyncTask<Todo, Unit, Unit>() {
+
+        override fun doInBackground(vararg p0: Todo?) {
+            todoDao.update(p0[0]!!)
+        }
+    }
+
+    private class DeleteCompletedTodosAsyncTask(val todoDao: TodoDao) :
+        AsyncTask<Unit, Unit, Unit>() {
+
+        override fun doInBackground(vararg p0: Unit?) {
+            todoDao.deleteCompleted()
+        }
+    }
+
+    private class DeleteAllAsyncTask(val todoDao: TodoDao) : AsyncTask<Unit, Unit, Unit>() {
+
+        override fun doInBackground(vararg p0: Unit?) {
+            todoDao.deleteAll()
+        }
+    }
+
+    private class DeleteTodoAsyncTask(val todoDao: TodoDao) : AsyncTask<Todo, Unit, Unit>() {
+
+        override fun doInBackground(vararg p0: Todo?) {
+            todoDao.delete(p0[0]!!)
+        }
     }
 }
